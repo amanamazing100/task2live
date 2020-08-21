dfee44d66c62c71ca9465ae5d28ae83f0519c61a
Aman Anand <53084393+amanamazing100@users.noreply.github.com>
2020-08-21 23:43:16 +0530

    Update filters.py

--- recordbook/filters.py
+++ recordbook/filters.py
@@ -3,8 +3,8 @@ from django_filters import CharFilter
 from .models import *
 
 class SearchFilter(django_filters.FilterSet):
-	name = CharFilter(field_name='name', lookup_expr='icontains')
-	roll = CharFilter(field_name='roll', lookup_expr='icontains')
+	#name = CharFilter(field_name='name', lookup_expr='icontains')
+	#roll = CharFilter(field_name='roll', lookup_expr='icontains')
 	class Meta:
 		model = Student
 		fields = '__all__'
